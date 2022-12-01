f2cdf5d586f146949a5a34e390afea56e5f72245
Abhishek Kumar <ak723215@gmail.com>
2022-12-01 16:01:46 +0530

    Initial Commit

---
+++ frontend/src/screens/OrderHistoryScreen.jsx
@@ -0,0 +1,106 @@
+import axios from 'axios';
+import React, { useReducer } from 'react';
+import { useEffect } from 'react';
+import { useContext } from 'react';
+import { Helmet } from 'react-helmet-async';
+import { useNavigate } from 'react-router-dom';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import { Store } from '../store';
+import { getError } from '../utils';
+import Button from 'react-bootstrap/Button';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, orders: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+const OrderHistoryScreen = () => {
+  const { state } = useContext(Store);
+  const { userInfo } = state;
+  const navigate = useNavigate();
+
+  const [{ loading, error, orders }, dispatch] = useReducer(reducer, {
+    loading: true,
+    error: '',
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const { data } = await axios.get(`/api/orders/mine`, {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        });
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+      } catch (error) {
+        dispatch({
+          type: 'FETCH_FAIL',
+          payload: getError(error),
+        });
+      }
+    };
+    fetchData();
+  }, [userInfo]);
+
+  return (
+    <div>
+      <Helmet>
+        <title>Order History</title>
+      </Helmet>
+      <h1 className="my-3">Order History </h1>
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>DATE</th>
+              <th>Total</th>
+              <th>PAID</th>
+              <th>DELIVERED</th>
+              <th>ACTIONS</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((item) => (
+              <tr key={item._id}>
+                <td>{item._id}</td>
+                <td>{item.createdAt.substring(0, 10)}</td>
+                <td>{item.totalPrice.toFixed(2)}</td>
+                <td>{item.isPaid ? item.paidAt.substring(0, 10) : 'No'}</td>
+                <td>
+                  {item.isDelivered ? item.deliveredAt.substring(0, 10) : 'No'}
+                </td>
+                <td>
+                  <Button
+                    type="button"
+                    variant="light"
+                    onClick={() => {
+                      navigate(`/order/${item._id}`);
+                    }}
+                  >
+                    Details
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
+
+export default OrderHistoryScreen;
